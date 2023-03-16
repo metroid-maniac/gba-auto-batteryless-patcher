b6cb85c15198972ea39198ba24b39bc96dc6b4e1
Metroid Maniac <trmetroidmaniac@gmail.com>
2023-03-16 09:38:01 +0000

    Update payload.s

--- payload.s
+++ payload.s
@@ -34,6 +34,7 @@ write_sram_patched:
     mov r3, # 0
     add r2, r0
 write_sram_patched_loop:
+    # Check if the each byte to write to sram is different - if it is, write it then set a flag
     ldrb r4, [r0]
     ldrb r5, [r1]
     cmp r4, r5
@@ -45,16 +46,19 @@ write_sram_patched_loop:
     cmp r0, r2
     blo write_sram_patched_loop
 
+    # If the flag was not set, the function had no effect. Short circuit
     cmp r3, # 0
     beq write_sram_patched_exit
 
+    # Install countdown irq handler and initialise countdown value
     adr r0, countdown_irq_handler
     mov r1, # 0x04
     lsl r1, # 24
     sub r1, # 0x10
     mov r2, # 255
     strh r2, [r1, # 0x0a]
-    str r0, [r1, # 0x04]
+    str r0, [r1, # 0x0c]
+    # Set green swap as a visual indicator that the countdown has begun
     strh r2, [r1, # 0x12]
     
 write_sram_patched_exit:
@@ -76,8 +80,25 @@ countdown_irq_handler:
     strh r1, [r0, # - 6]
     ldreq pc, [r0, # -12]
 
-    # countdown expired - flush sram to flash
-    b .
+    # countdown expired. time to flush sram to flash
+    # first switch back into user mode with interrupts still masked so there is enough stack.
+    # also disable sound.
+    mrs r1, cpsr
+    mrs r2, spsr
+    ldrb r3, [r0, # 0x84]
+    push {r0, r1, r2, r3, lr}
+    mov r1, # 0x009f
+    msr cpsr, r1
+    
+    pop {r0, r1, r2, r3, lr}
+    msr cpsr, r1
+    msr spsr, r2
+    strb r3, [r0, # 0x84]
+    
+    # Disable green swap and reinstall idle irq
+    strh r0, [r0, # 0x02]
+    adr r1, idle_irq_handler
+    str r1, [r0, # - 4]
 
 idle_irq_handler:
     ldr pc, [r0, # -12]
